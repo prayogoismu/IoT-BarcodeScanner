8ca039aefa9864044cd241eb1fce2c4d8879effb
prayogoismu <prayogoismu@gmail.com>
2018-12-09 20:28:08 +0700

    Add files via upload

---
+++ program/Arduino_Baca_Barcode.ino
@@ -0,0 +1,100 @@
+#include <usbhid.h>
+#include <usbhub.h>
+#include <hiduniversal.h>
+#include <hidboot.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+
+
+SoftwareSerial esp8266(7, 6);
+String data_barcode="";
+String receivedChar="";
+
+class MyParser : public HIDReportParser {
+  public:
+    MyParser();
+    void Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf);
+  protected:
+    uint8_t KeyToAscii(bool upper, uint8_t mod, uint8_t key);
+    virtual void OnKeyScanned(bool upper, uint8_t mod, uint8_t key);
+    virtual void OnScanFinished();
+};
+
+MyParser::MyParser() {}
+
+void MyParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf) {
+  // If error or empty, return
+  if (buf[2] == 1 || buf[2] == 0) return;
+
+  for (uint8_t i = 7; i >= 2; i--) {
+    // If empty, skip
+    if (buf[i] == 0) continue;
+
+    // If enter signal emitted, scan finished
+    if (buf[i] == UHS_HID_BOOT_KEY_ENTER) {
+      OnScanFinished();
+    }
+
+    // If not, continue normally
+    else {
+      // If bit position not in 2, it's uppercase words
+      OnKeyScanned(i > 2, buf, buf[i]);
+    }
+
+    return;
+  }
+}
+
+uint8_t MyParser::KeyToAscii(bool upper, uint8_t mod, uint8_t key) {
+  // Letters
+  if (VALUE_WITHIN(key, 0x04, 0x1d)) {
+    if (upper) return (key - 4 + 'A');
+    else return (key - 4 + 'a');
+  }
+
+  // Numbers
+  else if (VALUE_WITHIN(key, 0x1e, 0x27)) {
+    return ((key == UHS_HID_BOOT_KEY_ZERO) ? '0' : key - 0x1e + '1');
+  }
+
+  return 0;
+}
+
+void MyParser::OnKeyScanned(bool upper, uint8_t mod, uint8_t key) {
+  uint8_t ascii = KeyToAscii(upper, mod, key);
+  //Serial.print((char)ascii);
+  data_barcode=data_barcode+(char)ascii;
+  
+}
+
+void MyParser::OnScanFinished() {
+  Serial.print("BARCODE DATA= ");
+  Serial.println(data_barcode);
+  esp8266.println(data_barcode);
+  data_barcode="";
+}
+
+USB          Usb;
+USBHub       Hub(&Usb);
+HIDUniversal Hid(&Usb);
+MyParser     Parser;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin( 9600 );
+  Serial.println("Start");
+  esp8266.begin(9600);
+
+  if (Usb.Init() == -1) {
+    Serial.println("OSC did not start.");
+  }
+
+  delay( 200 );
+
+  Hid.SetReportParser(0, &Parser);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Usb.Task();
+}
